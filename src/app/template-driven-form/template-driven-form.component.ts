b66f87416ff84ace00799aa601bca0bc3fcf3e48
“roshnikeee” <roshnikeee@gmail.com>
2024-02-03 22:42:55 +0530

    merge conficts resolved

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,28 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
   templateUrl: './template-driven-form.component.html',
-  styleUrls: ['./template-driven-form.component.css']
+  styleUrls: ['./template-driven-form.component.css'],
 })
-export class TemplateDrivenFormComponent {
-countryList:Country[]=[
-  new Country('1','India'),
-  new Country('2','UK'),
-  new Country('3','Germany'),
-  new Country('4','Singapore')
-]
-onSubmit(form:NgForm){
-  
-}
-}
-class Country{
-  id:string;
-  name:string;
+export class TemplateDrivenFormComponent implements OnInit {
+  countryList: Country[] = [
+    new Country('1', 'India'),
+    new Country('2', 'USA'),
+    new Country('3', 'UK'),
+  ];
 
-  constructor(id:string,name:string){
-    this.id=id;
-    this.name=name;
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
+  contact!: Contact;
+  ngOnInit(): void {
+    this.contact = {
+      firstName: 'Sachin',
+      lastName: 'Tendulkar',
+      email: 'sachin@gmail.com',
+      gender: 'male',
+      isMarried: true,
+      country: '2',
+      address: { city: 'Mumbai', street: 'Perry Cross Rd', pincode: '400050' },
+    };
+  }
+}
+class Country {
+  id: string;
+  name: string;
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
+class Contact {
+  firstName!: string;
+  lastName!: string;
+  email!: string;
+  gender!: string;
+  isMarried!: boolean;
+  country!: string;
+  address!: {
+    city: string;
+    street: string;
+    pincode: string;
   }
 }
